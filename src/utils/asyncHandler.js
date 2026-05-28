9a068fb80c0c34a08950f18bba4ee55a19b0c5ab
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-05-28 17:43:32 +0530

    async handler created

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,18 @@
-export const asyncHandler = (reqHandler) => {
+const asyncHandler = (reqHandler) => {
                                     return (req, res, next) => {
                                         Promise.resolve(reqHandler(req, res, next))
                                                 .catch((error) => next(error));
                                     }
                             }
 export {asyncHandler};
+
+
+/**
+reqhandler created & passes to asyncHandler then it passes to middleware Or controller
+Example:
+const fn = () => {};
+const getMethod = asyncHandler(fn);
+-THE FINAL METHOD WHICH EXECUTED
+getMethod(req, res, next);
+ * 
+ */
