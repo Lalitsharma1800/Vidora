12db463aaa133f4152d172c053a41f2c166d3c36
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-05-29 00:27:15 +0530

    console added in api response

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -8,6 +8,7 @@ class ApiResponse{
         this.data = data;
         this.message = message;
         this.success = statusCode < 400;
+        console.log("Response send successfully"+String.fromCodePoint(0x1F60D));
     }
 }
 export {ApiResponse};
