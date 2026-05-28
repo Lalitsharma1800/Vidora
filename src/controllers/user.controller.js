f3f81e161b25421bc2e7837082b6c5e99d186687
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-05-29 00:24:39 +0530

    bug fiexed - registerUser controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,54 +4,54 @@ import {ApiResponse} from "./../utils/apiResponse.js";
 import {User} from "./../model/username.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-const registerUser = asyncHandler(async (req, res) => {
+const registerUser = asyncHandler(
+    
+    async (req, res) => {
 
-                                    res.status(200).json({message: "OK"});
+        // Get the data from the req
+        const {userName, email, fullName, password} = req.body;
 
+        //validate the fields (must not be empty)
+        if([userName, email, fullName, password].some((fields) => fields.trim() === "")){
+            throw new ApiError(401,"All fields are required");
+        };
 
-// Get the data from the req
-const {userName, email, fullName, password} = req.body;
+        //validate the user Existed or not
+        const existedUser = await User.findOne({
+                $or: [{userName},{email}]
+            })
+        if(existedUser) {
+            throw new ApiError(409, "User already existed")
+        };
 
-//validate the fields (must not be empty)
-if(
-    [userName, email, fullName, password].some((fields) => fields.trim() === "")
-){
-   throw new ApiError(401,"All fields are required");
-};
+        //check for image, avatar
+        const avatarLocalPath = req.files?.avatar[0]?.path;
+        if(!avatarLocalPath) throw new ApiError(400, "Avatar file is required");
 
-//validate the user Existed or not
-const existedUser = await User.findOne({
-    $or: [{userName},{email}]
-})
-if(existedUser) throw new ApiError(409, "User already existed");
+        let coverImageLocalPath;
+        if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) coverImageLocalPath = req.files.coverImage[0].path;
 
-//check for image, avatar
-const avatarLocalPath = req.files?.avatar[0]?.path;
-if(!avatarLocalPath) throw new ApiError(400, "Avatar file is required");
+        // upload documents on cloudinary
+        const avatar = await uploadOnCloudinary(avatarLocalPath);
+        let coverImage;
+        if(coverImageLocalPath){
+            coverImage = await uploadOnCloudinary(coverImageLocalPath);
+        };
 
-let coverImageLocalPath;
-if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) coverImageLocalPath = req.files.coverImage[0].path;
+        if(!avatar) throw new ApiError(400, "Avatar file is required");
 
-// upload documents on cloudinary
-const avatar = await uploadOnCloudinary(avatarLocalPath);
-const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+        // uploading details to db
+        const user = await User.create({userName: userName.toLowerCase(), fullName, email, password, avatar: avatar.url, coverImage: coverImage?.url || ""})
 
-if(!avatar) throw new ApiError(400, "Avatar file is required");
+        // validate - is user created successfully
+        const createdUser = await User.findById(user._id).select(
+                                    "-password -refreshToken"
+                                )
 
-// uplading details to db
-const user = await User.create({userName: userName.tolowerCase(), fullName, email, password, avatar: avatar.url, coverImage: coverImage?.url || ""})
+        if (!createdUser) {
+            throw new ApiError(500, "Something went wrong while registering the user")
+        }
 
-// validate - is user created successfully
-const userCreated = await User.findById(user._id).select(
-    "-password -refreshToken"
-)
-
-if (!createdUser) {
-    throw new ApiError(500, "Something went wrong while registering the user")
-}
-
-return res.status(201).json(
-    new ApiResponse(200, createdUser, "User registered Successfully")
-)
-                                });
+        return  res.status(201).json(new ApiResponse(200, createdUser, "User registered Successfully"))
+        });
 export {registerUser};
