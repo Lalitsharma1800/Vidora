ac56a430ae93dc587e55658dad6012561378d78f
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-01 01:12:22 +0530

    feature - add getCurrentUser routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, logout, changePassword} from "./../controllers/user.controller.js"
+import {registerUser, loginUser, logout, changePassword, getCurrentUser, updateAccountDetail} from "./../controllers/user.controller.js"
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyUser } from "../middleware/auth.middleware.js";
 
@@ -20,12 +20,12 @@ router.route("/register").post(
                             registerUser
                         );
 
-router.route("/login").post(
-    loginUser
-)
+router.route("/login").post(loginUser)
 
 // securec routes
 router.route("/logout").post(verifyUser, logout);
 router.route("/changePassword").post(verifyUser, changePassword);
+router.route("/getDetails").post(verifyUser, getCurrentUser);
+
 
 export default router;
