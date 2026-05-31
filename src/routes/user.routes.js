dbaee7e54550a86fc95e52f91ae07b8a741a5a58
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-01 01:09:33 +0530

    feature - add change password route to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser, loginUser, logout} from "./../controllers/user.controller.js"
+import {registerUser, loginUser, logout, changePassword} from "./../controllers/user.controller.js"
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyUser } from "../middleware/auth.middleware.js";
 
@@ -26,5 +26,6 @@ router.route("/login").post(
 
 // securec routes
 router.route("/logout").post(verifyUser, logout);
+router.route("/changePassword").post(verifyUser, changePassword);
 
 export default router;
