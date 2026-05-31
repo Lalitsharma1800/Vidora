c761ec0b37719b589fac4669f6cdb6c4ce1615e4
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-05-31 15:11:20 +0530

    logout route added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import {registerUser} from "./../controllers/user.controller.js"
+import {registerUser, loginUser, logout} from "./../controllers/user.controller.js"
 import { upload } from "../middleware/multer.middleware.js";
+import { verifyUser } from "../middleware/auth.middleware.js";
 
 
 const router = Router();
@@ -18,5 +19,12 @@ router.route("/register").post(
                             ]),
                             registerUser
                         );
+
+router.route("/login").post(
+    loginUser
+)
+
+// securec routes
+router.route("/logout").post(verifyUser, logout);
 
 export default router;
