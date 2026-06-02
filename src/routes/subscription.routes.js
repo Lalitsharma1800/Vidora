b364150c95bb37375516ccb1fe66e0c312c17216
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-02 23:48:58 +0530

    feat - update subscription routes to include getSubscriberList and getSubscribedToList functionalities

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyUser } from "../middleware/auth.middleware.js";
-import { toggle_subscribe } from "../controllers/subscription.controller.js";
+import { toggle_subscribe, getSubscriberList, getSubscribedToList } from "../controllers/subscription.controller.js";
 
 const router = Router();
 
 router.use(verifyUser)
-router.route("/:id").get(toggle_subscribe)
-                    .post(toggle_subscribe)
+router.route("/:id").post(toggle_subscribe);
+router.route("/subscribers").get(getSubscriberList);
+router.route("/subscribedChannels").get(getSubscribedToList);
 
 export default router;
