9a70a5a37a2fb06805241dcd821c3d3ed4c298d8
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-02 20:04:23 +0530

    fix - wrap access token verification in try-catch block in auth.middleware.js

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,17 +6,21 @@ import { User } from "../model/username.model.js";
 
 export const verifyUser = asyncHandler(async (req, res, next) => {
 
-        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-        
-        if(!accessToken) throw new ApiError(401, "Unauthorised request.");
+        try {
+                        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+                        
+                        if(!accessToken) throw new ApiError(401, "Unauthorised request");
 
-        const payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-
-        const user = await User.findById(payload._id).select("-password -refreshToken");
-
-        if(!user) throw new ApiError(401, "Invalid Access Token");
-        
-        req.user = user;
-
-        next();
+                        const payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+                
+                        const user = await User.findById(payload?._id).select("-password -refreshToken");
+                
+                        if(!user) throw new ApiError(401, "Invalid Access Token");
+                        
+                        req.user = user;
+                
+                        next();
+        } catch (error) {
+                throw new ApiError(401, error.message || "Invalid access token");
+        };
 });
