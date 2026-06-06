650567f207671a6fefe6f25499dfe9394d4bf9f3
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-06 19:26:04 +0530

    fix: enhance subscription handling with transaction support and improve error handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,45 +9,54 @@ const toggle_subscribe = asyncHandler(async (req, res) => {
     
     const {id} = req.params;
     if(!id || id === `""` || id === ":id") throw new ApiError(400, "channel required");
-
-    const channel = await User.findById(id);
+    const session = await mongoose.startSession();
+    try {
+    session.startTransaction()
+    
+    const channel = await User.findById(id).session(session);
     if(!channel) throw new ApiError(400, "channel not found");
     if(channel._id.equals(req.user._id)) throw new ApiError(400, "Own channel cannot subscibed");
 
-    const isSubscribed = await Subscription.findOne({"channel" : channel._id, "subscriber" : req.user._id});
+    const isSubscribed = await Subscription.findOne({"channel" : channel._id, "subscriber" : req.user._id}).session(session);
     let toggle_subscription;
     
     if(isSubscribed){
-        toggle_subscription = await Subscription.findByIdAndDelete(isSubscribed._id);
+        toggle_subscription = await Subscription.findByIdAndDelete(isSubscribed._id).session(session);
         if(!toggle_subscription) throw new ApiError(500, "Something went wrong while Unsubscribing");
         return res
                     .status(200)
                     .json(new ApiResponse(200, "channel Unsubscribed successfully"));
     };
-    toggle_subscription = await Subscription.create({"channel": channel._id, "subscriber" : req.user._id});
-    if(!toggle_subscription) throw new ApiError(500, "Something went wrong while subscribing");
-
+    toggle_subscription = await Subscription.create({"channel": channel._id, "subscriber" : req.user._id}).session(session);
+    if(!toggle_subscription) throw new ApiError(500, "Something went wrong while subscribing").session(session);
+    await session.commitTransaction();
     return res
                 .status(200)
                 .json(new ApiResponse(200, "channel subscribed successfully"));
+    } catch (error) {
+        await session.abortTransaction();
+        throw new ApiError(500, "something went wrong while subscription");
+    }
+    finally{
+        await session.endSession();
+    }
 });
 
 const getSubscriberList = asyncHandler(async (req, res) => {
     const user = req.user;
 
     const subscriberList = await Subscription.find({channel: user._id}).select("-channel");
-    console.log(subscriberList);
-    const count = subscriberList.length;
+    const count = await User.findById(user._id).select("subscriberCount");
     return res
                 .status(200)
-                .json(new ApiResponse(200, {count, subscriberList}, "subscribers fetched successfully"))
+                .json(new ApiResponse(200, {count, subscriberList}, "subscribers fetched successfully"));
 });
 
 const getSubscribedToList = asyncHandler(async (req, res) => {
     const user = req.user;
 
     const subscribedToList = await Subscription.find({subscriber: user._id}).select("-subscriber");
-    const count = subscribedToList.length;
+    const count = await User.findById(user._id).select("subscribedToCount");
     return res
                 .status(200)
                 .json(new ApiResponse(200, {count, subscribedToList}, "following fetched successfully"))
