75dc3f3167239af060877be2f05448cf5cfdccc5
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-07 10:19:26 +0530

    feat: add LikeVideo model for handling video reactions

---
+++ src/model/like.model.js
@@ -0,0 +1,30 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const likeVideoSchema = new Schema(
+                    {
+                        videoId: {
+                            type: mongoose.Types.ObjectId,
+                            required: true
+                        },
+                        userId: {
+                            type: mongoose.Types.ObjectId,
+                            required: true
+                        },
+                        reaction: {
+                            type: String,
+                            enum: {
+                                values: ['LIKE', 'DISLIKE'],
+                                message: '{VALUE} is not a valid reaction type'
+                            },
+                            required: true
+                        }
+                    },
+                    {
+                        timestamps: true
+                    }
+);
+mongoose.plugin(mongooseAggregatePaginate);
+
+export const LikeVideo = mongoose.model("LikeVideo", likeVideoSchema);
