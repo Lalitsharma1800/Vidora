66585e4243fd3ee882f1b2fbcac9520945c305ba
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-09 17:25:05 +0530

    feat: add CommentReply model with schema for replies to comments

---
+++ src/model/comment_reply.model.js
@@ -0,0 +1,37 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const CommentReplySchema = new Schema(
+                    {
+                        commentId: {
+                            type: mongoose.Types.ObjectId,
+                            required: true
+                        },
+                        userId: {
+                            type: mongoose.Types.ObjectId,
+                            required: true
+                        },
+                        likes: {
+                            type: Number,
+                            default: 0
+                        },
+                        disLike: {
+                            type: Number,
+                            default: 0       
+                        },
+                        reply: {
+                            type: String,
+                            required: true
+                        },
+                        replyCount: {
+                            type: Number,
+                            default: 0
+                        }
+                    },
+                    {
+                        timestamps: true
+                    }
+);
+mongoose.plugin(mongooseAggregatePaginate);
+
+export const Comment_Reply = mongoose.model("CommentReply", CommentReplySchema);
