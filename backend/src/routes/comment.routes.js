3cc41c8dcb7606137741522abbf761d24b18343a
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-12 16:57:25 +0530

    feat: implement comment and reply routes with user verification

---
+++ backend/src/routes/comment.routes.js
@@ -0,0 +1,27 @@
+import Router from "express";
+import {verifyUser} from "../middleware/auth.middleware.js";
+import {
+    writeComment,
+    editComment,
+    findComments,
+} from "../controllers/comment.controller.js";
+import {
+    writeReply,
+    editReply,
+    findReplies,
+} from "../controllers/reply_comment.controller.js";
+
+
+const router = Router();
+
+// unverified routes
+router.get("/findcomments", findComments);
+router.get("/findreplies", findReplies);
+
+// verified routes
+router.post("/writecomment", verifyUser, writeComment);
+router.post("/writereply", verifyUser, writeReply);
+router.put("/editcomment",verifyUser, editComment);
+router.put("/editreply", verifyUser, editReply);
+
+export default router;
