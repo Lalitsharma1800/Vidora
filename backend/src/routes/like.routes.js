c89df869650e0e66e63ae90f4ac9ce0e05d69571
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-12 16:57:18 +0530

    feat: enhance like routes with additional endpoints for comments and replies

--- backend/src/routes/like.routes.js
+++ backend/src/routes/like.routes.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyUser } from "../middleware/auth.middleware.js";
-import { reactOnVideo, reactStatus } from "../controllers/like.controller.js";
+import { 
+    reactOnVideo, 
+    reactStatus, 
+    reactOnComment, 
+    reactStatusOnComments, 
+    reactOnReply 
+} from "../controllers/like.controller.js";
 
 const router = Router();
 
-router.route("").post(verifyUser, reactOnVideo);
-router.route("").get(verifyUser,reactStatus);
+router.route("/video").post(verifyUser, reactOnVideo);
+router.route("/video").get(verifyUser,reactStatus);
+router.route("/comment").post(verifyUser, reactOnComment);
+router.route("/comment").get(verifyUser,reactStatusOnComments);
+router.route("/reply").post(verifyUser, reactOnReply);
+router.route("/reply").get(verifyUser,reactStatusOnComments);
 
 export default router;
