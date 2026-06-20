d9b84df9e9e8411e9618c9b85457f74af47cb5ef
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-20 21:50:19 +0530

    sidebar active state completed

--- frontend/src/components/layout/sidebar/SidebarItem.tsx
+++ frontend/src/components/layout/sidebar/SidebarItem.tsx
@@ -1,5 +1,7 @@
-import type { SidebarItemData } from './sidebar-data.ts'
-import { cn } from '@/lib/utils'
+import type { SidebarItemData } from './sidebar-data.ts';
+import { Link } from '@tanstack/react-router';
+import { cn } from '@/lib/utils';
+import { useSidebarStore } from '#/zustand/sidebarStore/useSidebar.ts';
 
 export interface SidebarItemProps {
   item: SidebarItemData
@@ -8,39 +10,43 @@ export interface SidebarItemProps {
 
 export function SidebarItem({ item, collapsed = false }: SidebarItemProps) {
   const Icon = item.icon
-
+  const activeSidebar = useSidebarStore((state) => state.activeSidebar);
+  const setActiveSidebar = useSidebarStore((state) => state.setActiveSidebar);
+  const onClickHandler = (sidebar : String) => {
+          setActiveSidebar(sidebar);    
+  };
   return (
-    <li>
-      <a
-        href={item.href}
-        aria-label={item.label}
-        aria-current={item.isActive ? 'page' : undefined}
-        className={cn(
-          'group flex min-h-10 items-center rounded-xl text-sm font-normal  transition-colors duration-200',
-          'hover:bg-sidebar-accent hover:text-neutral-900',
-          'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sidebar-ring focus-visible:ring-offset-2 focus-visible:ring-offset-sidebar',
-          item.isActive &&
-            'bg-sidebar-accent font-medium',
-          collapsed
-            ? 'mx-auto size-10 justify-center px-0'
-            : 'gap-5 px-3 py-2',
-        )}
-      >
-        <Icon
-          className={cn(
-            'size-5 shrink-0 transition-transform duration-200 group-hover:scale-105 text-white'
-          )}
-          aria-hidden="true"
-        />
-        <span
-          className={cn(
-            'truncate  text-white', item.isActive && 'font-bold',
-            collapsed && 'sr-only',
-          )}
-        >
-          {item.label}
-        </span>
-      </a>
+    <li onClick={() => onClickHandler(item.id)}>
+          <Link
+            to={item.href}
+            aria-label={item.label}
+            aria-current={item.isActive ? 'page' : undefined}
+              className={cn(
+                'group flex min-h-10 items-center rounded-xl text-sm font-normal  transition-colors duration-200',
+                'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sidebar-ring focus-visible:ring-offset-2 focus-visible:ring-offset-sidebar',
+                `${(item.id !== activeSidebar) && 'hover:bg-sidebar-accent hover:text-neutral-500' }`,
+                item.id === activeSidebar &&
+                'bg-neutral-800 font-medium',
+                collapsed
+                  ? 'mx-auto size-10 justify-center px-0 text-white'
+                  : 'gap-5 px-3 py-2 text-white',
+              )}
+            >
+              <Icon
+                className={cn(
+                  'size-5 shrink-0 transition-transform duration-200 group-hover:scale-105 text-white'
+                )}
+                aria-hidden="true"
+              />
+              <span
+                className={cn(
+                  'truncate  ', item.isActive && 'font-bold',
+                  collapsed && 'sr-only',
+                )}
+              >
+                {item.label}
+              </span>
+          </Link>
     </li>
   )
 }
