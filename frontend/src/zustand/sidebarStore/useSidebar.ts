3b6ebeec645a929727f0c75656fe9bd56139b6e6
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-20 23:22:06 +0530

    store created

--- frontend/src/zustand/sidebarStore/useSidebar.ts
+++ frontend/src/zustand/sidebarStore/useSidebar.ts
@@ -1,16 +1,11 @@
 import { create } from 'zustand';
 
-export type SidebarTab = 
-        'home' | 'playlist' | 'comment' | 'subscriptions' | 'watch-later' | 'liked' | 
-        'comment' | 'trending' | 'music' | 'gaming' | 'news' | 'sports' | 'settings' | 
-        'help' | 'feedback';
-
 interface SidebarState {
-  activeSidebar: SidebarTab;
-  setActiveSidebar: (sidebar: SidebarTab) => void;
+  activeSidebar: String;
+  setActiveSidebar: (sidebar: String) => void;
 }
 
 export const useSidebarStore = create<SidebarState>((set) => ({
   activeSidebar: 'home',
-  setActiveSidebar: (sidebar: SidebarTab) => set({ activeSidebar: sidebar }), 
+  setActiveSidebar: (sidebar: String) => set({ activeSidebar: sidebar }), 
 }));
