4443d0e3cddc0652d080acf4b5e348830aa4f2e4
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-25 16:16:35 +0530

    Remove YouTube component files and related README documentation

--- frontend/src/components/youtube/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as YouTubeWatch } from './YouTubeWatch';
-export { default as VideoPlayer } from './VideoPlayer';
-export { default as VideoDetails } from './VideoDetails';
-export { default as CommentsSection } from './CommentsSection';
-export { default as RecommendedVideos } from './RecommendedVideos';
