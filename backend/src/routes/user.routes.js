bda7ecb056d9338618497ea7126d7f4c277c7f4b
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-26 17:41:22 +0530

    Enhance user routes by integrating separate video and thumbnail upload handlers, improving upload functionality and maintaining user verification.

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { upload } from "../middleware/multer.middleware.js";
+import { upload, upload_video_and_thumbnail } from "../middleware/multer.middleware.js";
 import { verifyUser } from "../middleware/auth.middleware.js";
 import {
         registerUser, 
@@ -14,7 +14,8 @@ import {
         getChannelProfile,
         watchHistory
     } from "./../controllers/user.controller.js";
-import { uploadVideo, getFeed, saveInHistory, changeThumbnail, getVideo } from "../controllers/video.controller.js";
+import {saveInHistory} from "../controllers/watch_history.controller.js";
+import { upload_video,upload_thumbnail, getFeed, getVideo } from "../controllers/video.controller.js";
 import { getSubscriberCountAndSubscribedStatus } from "../controllers/subscription.controller.js";
 
 const router = Router();
@@ -42,20 +43,8 @@ router.route("/video/:id").get(getVideo);
 // securec routes
 router.route("/logout").post(verifyUser, logout);
 router.route("/changePassword").post(verifyUser, changePassword);
-router.route("/uploadVideo").post(
-                            upload.fields([
-                                {
-                                    name: "video",
-                                    maxCount: 1
-                                },
-                                {
-                                    name: "thumbnail",
-                                    maxCount: 1
-                                }
-                            ]),
-                            verifyUser,
-                            uploadVideo
-                        );
+router.route("/uploadVideo").post(upload_video_and_thumbnail.single("video"),verifyUser,upload_video);
+router.route("/upload-thumbnail").post(upload_video_and_thumbnail.single("thumbnail"),verifyUser,upload_video);
 
 router.route("/save").post(verifyUser, saveInHistory);
 router.route("/home").post(verifyUser,getSubscriberCountAndSubscribedStatus);
@@ -67,7 +56,7 @@ router.route("/history").get(verifyUser, watchHistory);
 router.route("/updateAccountDetail").patch(verifyUser, updateAccountDetail);
 router.route("/updateAvatar").patch(upload.single("avatar"), verifyUser, updateAvatar);
 router.route("/updateCoverImage").patch(upload.single("coverImage"), verifyUser, updateCoverImage);
-router.route("/changeThumbnail").patch(upload.single("thumbnail"), verifyUser, changeThumbnail);
+router.route("/changeThumbnail").patch(upload_video_and_thumbnail.single("thumbnail"), verifyUser, upload_thumbnail);
 
 
 export default router;
