93288b84a5544ea8f6491f7e7d081d78d7cf88e8
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-26 17:41:12 +0530

    Refactor video upload functionality to support chunked uploads and separate thumbnail uploads, enhancing error handling and database transaction management.

--- backend/src/controllers/video.controller.js
+++ backend/src/controllers/video.controller.js
@@ -1,51 +1,77 @@
 import mongoose from "mongoose";
-import { uploadVideoOnCloudinary, uploadOnCloudinary} from "../utils/cloudinary.js";
+import { uploadChunkToCloudinary, upload_Thumbnail_To_Cloudinary} from "../utils/cloudinary.js";
 import { User } from "../model/username.model.js";
-import { Subscription } from "../model/subscription.model.js";
 import {Video} from ".././model/video.model.js";
-import { WatchHistory } from "../model/watchhistory.model.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 
+/**
+=======================
+ only video upload
+=======================
+ */
+const upload_video = asyncHandler(async (req, res) => {
+    
+    const {title, description, upload_id} = req.body;
 
+    if (!title || !uploadId) throw new ApiError(400, "Title and uploadId are required");
 
-const uploadVideo = asyncHandler(async (req, res) => {
-    const user = req.user;
-    const {title, description} = req.body;
-    if(!title) throw new ApiError(400, "title not provided");
-    const videoFile = req.files?.video[0]?.path;
-    if(!videoFile) throw new ApiError(401, "Please upload video");
-    const thumbnailFile = req.files?.thumbnail[0]?.path;
-    if(!thumbnailFile) throw new ApiError(401, "Please upload thumbnail");  
-    const uploaded_video = await uploadOnCloudinary(videoFile);
-    if(!uploaded_video.url) throw new ApiError(500, "something went wrong while uploading the video file to cloudinary");
-    const thumbnail = await uploadOnCloudinary(thumbnailFile);
-    if(!thumbnail.url) throw new ApiError(500, "something went wrong while uploading the video file to cloudinary");
-    const video = await Video.create({videoFile: uploaded_video.url, thumbnail: thumbnail.url, title: title, description: description || "", duration: uploaded_video.duration, owner: user._id});
-    if(!video) throw new ApiError(500, "something went wrong while uploding video to mongodb");
-    return res
-                .status(200)
-                .json(new ApiResponse(200,  [video], "video uploaded successfully"));
+    const video_file = req.file;
+
+    if (!video_file) throw new ApiError(400, 'Video chunk file is required!');
+
+    const video_result = await uploadChunkToCloudinary(video_file.buffer, upload_id);
+
+    if(video_result && video_file.done === true){
+        const session = await mongoose.startSession();
+
+        try {
+            await session.startTransaction();
+
+            const upload_video = await Video.create([{videoFile: video_result.secure_url, thumbnail: "", title: title, description: description || "", duration: video_result.duration, owner: user._id}], {session});
+            
+            if(!upload_video) throw new ApiError(500, "video upload failed!!");
+
+            const update_video_count = await User.findOneAndUpdate({_id: req.user._id}, {$inc: {totalVideos: 1}}, {returnDocument: "after", session});
+
+            if(!update_video_count) throw new ApiError(500, "video uploading process failed while updating the user's video count");
+            await session.commitTransaction();
+            return res.status(200)
+                      .json(new ApiResponse(200, {upload_video, update_video_count}, "video successfully uploaded"));
+        } catch (error) {
+            await session.abortTransaction();
+            throw new ApiError(500, "something went wrong while uploading the video!!");     
+        }
+        finally{
+            await session.endSession();
+        }
+    };
+    return res.status(200)
+              .josn(new ApiResponse(200, video_result, "chunk uploaded successfully"));
 });
-const changeThumbnail = asyncHandler(async (req, res) => {
-    const user = req.user;
-    const {videoId} = req.body;
-    if(!videoId) throw new ApiError("Please select any video");
-    const thumbnailFile = req.file?.path;
-    if(!thumbnailFile) throw new ApiError(401, "Please upload thumbnail");
+/**
+=======================
+ only thumbnail upload
+=======================
+ */
 
-    const video = await Video.findById(new mongoose.Types.ObjectId(videoId));
-    if(!video) throw new ApiError(400, "please upload a video first");
-    const thumbnail = await uploadOnCloudinary(thumbnailFile);
-    if(!thumbnail.url) throw new ApiError(500, "something went wrong while uploading the thumbnail file to cloudinary");
+const upload_thumbnail = asyncHandler(async (req, res) => {
 
-    const changedThumbnail = await Video.findByIdAndUpdate(new mongoose.Types.ObjectId(video._id),{$set: {thumbnail: thumbnail.url}})
-    if(!changedThumbnail) throw new ApiError(500, "thumbnail not changed please try again later");
-    return res
-                .status(200)
-                .json(new ApiResponse(200, null,"thumbnail changed successfully"))
+    const {video_id, upload_id} = req.body;
+
+    if(!video_id || !upload_id) throw new ApiError(400, "video_id & upload_id is required!!");
+    const thumbnail_file = req.file;
+    if (!thumbnail_file)  throw new ApiError(400,'thumbnail file required!!');
+    const thumbnail_result = await upload_Thumbnail_To_Cloudinary(thumbnail_file.buffer, upload_id);
+    
+    if (thumbnail_result && thumbnail_result.done === true) {
+        const upload_thumbnail = await Video.findOneAndUpdate({_id: new mongoose.Types.ObjectId(video_id)},{$set: {thumbnail: thumbnail_result.secure_url}})
+        if(!upload_thumbnail) throw new ApiError(500, "thumbnail upload failed!1");
+        return res.status(200)
+                  .json(new ApiResponse(200, upload_thumbnail, "thumbnail upload successfully"))
+    }
 });
 
 const getVideo = asyncHandler(async (req, res) => {
@@ -81,26 +107,13 @@ const getFeed = asyncHandler(async (req, res) => {
             }
         },
     ]);
-    console.log(videoData);
     return res
                 .status(200)
                 .json(new ApiResponse(200, videoData, "Home feed fetched"));
 });
 
-const saveInHistory = asyncHandler(async (req, res) => {
-    const {videoId} = req.body;
-    if(!mongoose.Types.ObjectId.isValid(videoId)) throw new ApiError(400, "Invalid video");
-    if(!mongoose.Types.ObjectId.isValid(req.user?._id)) throw new ApiError(400, "Invalid video");
 
-    const progress = req.progress || 0;
 
-    let history = await WatchHistory.findOneAndUpdate({videoId:new mongoose.Types.ObjectId(videoId), userId: req.user._id},{$set:{progress: progress, lastWatchedAt: new Date()}}, {returnDocument: "after", upsert: true});
 
-    if(!history) throw new ApiError(500, "Something went wrong while saving the video in history");
-    
-    return res
-                .status(200)
-                .json(new ApiResponse(200,null, "video saved in history successfully"))
-});
 
-export {uploadVideo, changeThumbnail, getFeed, saveInHistory, getVideo};
+export {upload_video, upload_thumbnail, getFeed, saveInHistory, getVideo};
