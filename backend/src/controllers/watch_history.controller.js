159046c15255ff2cf7f851893e82383d64fec08d
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-06-26 17:37:11 +0530

    Add watch history controller to handle video history saving functionality

---
+++ backend/src/controllers/watch_history.controller.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { ApiResponse } from "../utils/apiResponse";
+import { ApiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import { WatchHistory } from "../model/watchhistory.model";
+
+export const saveInHistory = asyncHandler(async (req, res) => {
+    const {videoId} = req.body;
+    if(!mongoose.Types.ObjectId.isValid(videoId)) throw new ApiError(400, "Invalid video");
+    if(!mongoose.Types.ObjectId.isValid(req.user?._id)) throw new ApiError(400, "Invalid video");
+
+    const progress = req.progress || 0;
+
+    let history = await WatchHistory.findOneAndUpdate({videoId:new mongoose.Types.ObjectId(videoId), userId: req.user._id},{$set:{progress: progress, lastWatchedAt: new Date()}}, {returnDocument: "after", upsert: true});
+
+    if(!history) throw new ApiError(500, "Something went wrong while saving the video in history");
+    
+    return res
+                .status(200)
+                .json(new ApiResponse(200,null, "video saved in history successfully"))
+});
