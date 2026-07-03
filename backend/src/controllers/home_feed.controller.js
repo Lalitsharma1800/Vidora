1e4e3f819d93157b06fcea9f73895a25b3be438e
Lalit Sharma <lalit.sharma18700@gmail.com>
2026-07-03 13:49:44 +0530

    update: get_feed optimized with pagination

--- backend/src/controllers/home_feed.controller.js
+++ backend/src/controllers/home_feed.controller.js
@@ -4,29 +4,66 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 export const getFeed = asyncHandler(async (req, res) => {
-    const videoData = await Video.aggregate([
-        {
-            $match: {isPublished: true}
-        },
-        {
-            $lookup: {
-                from: "users",
-                localField:"owner",
-                foreignField: "_id",
-                as: "channel",
-                pipeline: [
-                        {
-                            $project: {
-                                userName: 1,
-                                fullName: 1,
-                                avatar: 1
-                            }
-                        },               
-                ]
+
+  const page = parseInt(req.body.page, 10) || 1;
+  const limit = parseInt(req.body.limit, 10) || 10;
+
+  const mainPipeline = Video.aggregate([
+    {
+      $match: { isPublished: true }
+    },
+    {
+      $sort: { createdAt: -1 }
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "channel",
+        pipeline: [
+          {
+            $project: {
+              userName: 1,
+              fullName: 1,
+              avatar: 1
             }
-        },
-    ]);
-    return res
-                .status(200)
-                .json(new ApiResponse(200, videoData, "Home feed fetched"));
+          }               
+        ]
+      }
+    },
+    {
+      $unwind: {
+        path: "$channel",
+        preserveNullAndEmptyArrays: true
+      }
+    }
+  ]);
+
+  const countPipeline = Video.aggregate([
+    { $match: { isPublished: true } }
+  ]);
+
+  const options = {
+    page,
+    limit,
+    countAggregation: countPipeline, 
+    customLabels: {
+      docs: 'videos', 
+    }
+  };
+
+
+  const result = await Video.aggregatePaginate(mainPipeline, options);
+
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200, 
+        result,
+        "Home feed fetched successfully"
+      )
+    );
 });
